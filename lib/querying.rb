aebfb1c3fc4751af577cc1e8329b1779f1f1742a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 03:06:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
   #series.title character.species INNER JOIN series, author WHERE species = human limit 1
   "SELECT series.title
   FROM series
-  INNER JOIN characters ON characters.series-id = series.id
+  INNER JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human' LIMIT 1"
 end
 
