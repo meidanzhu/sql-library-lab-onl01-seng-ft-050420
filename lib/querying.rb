2f2c66be36c0d288a0e0cacf41e093a63b0ed8a7
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 03:26:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,7 @@ def select_series_title_with_most_human_characters
    JOIN characters ON series.author_id = characters.author_id
    WHERE characters.species = 'human'
    GROUP BY characters.species
-   ORDER BY COUNT(characters.species) DESC
+   ORDER BY COUNT(*) DESC
    LIMIT 1
    "
 end
