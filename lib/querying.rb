4aaa907af65f23b6c9be72956aca48e35c56d96b
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 03:09:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN characters ON series.id = characters.series_id
-  WHERE characters.species = 'human' LIMIT 1"
+  WHERE characters.species = 'human' 
+  ORDER BY characters.series_id DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
