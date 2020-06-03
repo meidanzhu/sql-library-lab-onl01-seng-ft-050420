5f0f38b40ccd9415a04eb1664b023129ac584f21
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 03:03:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   #characters.name character_books.book_id COUNT
-  "SELECT characters.name, COUNT(character_books.book_id)
+  "SELECT characters.name, COUNT(character_books.character_id)
   FROM characters
   JOIN character_books ON characters.character_books_id = characters.id
   GROUP BY characters.name ORDER BY COUNT(characters.id) DESC"
